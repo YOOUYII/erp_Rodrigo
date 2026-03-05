1d4a80a84bb3500db3ab86c04523fa00b7d09a6e
Rodrigo Vázquez Rico <rodririco213@gmail.com>
2026-03-04 18:32:36 -0600

    V0.4
    Se arreglaron errores en el registro

--- src/app/pages/auth/register/register.ts
+++ src/app/pages/auth/register/register.ts
@@ -9,7 +9,7 @@ import { InputTextModule } from 'primeng/inputtext';
 import { PasswordModule } from 'primeng/password';
 import { MessageModule } from 'primeng/message';
 
-import {ReactiveFormsModule,NonNullableFormBuilder,Validators} from '@angular/forms';
+import {ReactiveFormsModule,NonNullableFormBuilder,Validators, AbstractControl, ValidationErrors} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -44,18 +44,54 @@ private fb = inject(NonNullableFormBuilder);
   private readonly passwordPattern = /^(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{10,}$/;
 
   readonly registerForm = this.fb.group({
-    name: ['', Validators.required],
-    email: ['', [Validators.required, Validators.email]],
-    age: [, [Validators.required, Validators.min(18)]],
-    phone: ['', [
+    name: ['', [
+      Validators.required, 
+      this.noWhitesSpacesValidator
+    ]],
+    user: ['',[
       Validators.required,
+      Validators.pattern(/^[A-Za-z0-9_]{4,20}$/)
+    ]],
+    email: ['', [
+      Validators.required, Validators.email
+    ]],
+    age: [ , [
+      Validators.required, 
+      Validators.min(18), 
+      Validators.pattern(/^[0-9]+$/)
+    ]],
+    phone: ['', [ 
+      Validators.required, 
       Validators.pattern(/^[0-9]+$/)
     ]],
     password: ['', [
-      Validators.required,
+      Validators.required, 
       Validators.pattern(this.passwordPattern)
-    ]]
+    ]],
+    confirmPassword: ['', 
+      Validators.required
+    ]
+  },{
+    validators: this.passwordsMatchValidator // DEBE DE ESTAR ESTE AQUI PORUQE USA GET.(PASSWORD) SI ESTUVIERA SOLO EN EL CAMPO DE CONFIRMPASSWORD NO LO PODRIA OBTENER
   });
+
+  passwordsMatchValidator(control: AbstractControl): ValidationErrors | null { // AbstractControl representa todo el form. Si retorna null esta bien sino mal
+    const password = control.get('password')?.value; // SE PONE ASI PORQUE LA VALIDACION QUEDAA GLOBAL PARA TODO EL FORMULARIO
+    const confirmPassword = control.get('confirmPassword')?.value;
+
+    return password === confirmPassword ? null : { passwordMismatch: true };
+  }
+
+  noWhitesSpacesValidator(control: AbstractControl): ValidationErrors | null {
+
+    const name = control.value; // SE PONE ASI PORQUE ESTA VALIDACION SE ESTA PONIENDO DENTRO DEL CAMPO YA 
+    
+    if(name && name.trim().length === 0){
+      return {whiteSpace: true};
+    }
+
+    return null;
+  }
 
   register(): void {
     if (this.registerForm.invalid) return;
